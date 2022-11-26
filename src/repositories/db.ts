cc0c283ece1a3f40a11462e42ff738c4eade5de7
Georgy <Aegoraa@yandex.ru>
2022-11-27 00:23:54 +0300

    added env and gitignore

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,9 +1,15 @@
 import { MongoClient } from "mongodb";
 import { IBlogs } from "./blogs-db-repository";
 import { IPosts } from "./posts-db-repository";
-const mongoUri = "mongodb://localhost";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const client = new MongoClient(mongoUri);
+const mongoUrl = process.env.MONGO_URL;
+
+if (!mongoUrl) {
+  throw new Error("Can't connect to db");
+}
+const client = new MongoClient(mongoUrl);
 export const db = client.db("blog-api");
 export const blogsCollection = db.collection<IBlogs>("blogs", {});
 export const postsCollection = db.collection<IPosts>("posts");
