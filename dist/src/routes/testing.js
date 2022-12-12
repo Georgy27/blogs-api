2d6c3499c95dcf6be748af81896eeb92dd7acbaa
Georgy <Aegoraa@yandex.ru>
2022-12-13 01:26:36 +0300

    added clearUsers fix

--- dist/src/routes/testing.js
+++ dist/src/routes/testing.js
@@ -13,9 +13,11 @@ exports.testingRouter = void 0;
 const express_1 = require("express");
 const blogs_db_repository_1 = require("../repositories/blogs-db-repository");
 const posts_db_repository_1 = require("../repositories/posts-db-repository");
+const users_db_repository_1 = require("../repositories/users-db-repository");
 exports.testingRouter = (0, express_1.Router)({});
 exports.testingRouter.delete("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     yield blogs_db_repository_1.blogsRepository.clearBlogs();
     yield posts_db_repository_1.postsRepository.clearPosts();
+    yield users_db_repository_1.usersRepository.clearUsers();
     return res.sendStatus(204);
 }));
--- src/routes/testing.ts
+++ src/routes/testing.ts
@@ -1,11 +1,13 @@
 import { Request, Response, Router } from "express";
 import { blogsRepository } from "../repositories/blogs-db-repository";
 import { postsRepository } from "../repositories/posts-db-repository";
+import { usersRepository } from "../repositories/users-db-repository";
 
 export const testingRouter = Router({});
 
 testingRouter.delete("/", async (req: Request, res: Response) => {
   await blogsRepository.clearBlogs();
   await postsRepository.clearPosts();
+  await usersRepository.clearUsers();
   return res.sendStatus(204);
 });
