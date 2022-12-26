cd23354d6f7e3244c7cd2f12ca38d0887faee916
Georgy <Aegoraa@yandex.ru>
2022-12-27 01:02:47 +0300

    fixed email url

--- src/managers/emails-manager.ts
+++ src/managers/emails-manager.ts
@@ -3,7 +3,7 @@ import { UserAccountDBModel } from "../models/users-model/UserAccountDBModel";
 
 export const emailsManager = {
   async sendEmailConformationMessage(user: UserAccountDBModel) {
-    let message = `<h1>Thank for your registration</h1><p>To finish registration please follow the link below: <a href='http://localhost:3000/auth/registration-confirmation?code=${user.emailConfirmation.confirmationCode}'>complete registration</a> </p>`;
+    let message = `<h1>Thank for your registration</h1><p>To finish registration please follow the link below: <a href='https://blogs-api-ruby.vercel.app/registration-confirmation?code=${user.emailConfirmation.confirmationCode}'>complete registration</a> </p>`;
     await emailAdapter.sendEmail(user.accountData.email, message);
   },
 };
