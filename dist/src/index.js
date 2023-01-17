56b80c0c7076097885f06c02fdaadf8660dde4d6
Georgy <Aegoraa@yandex.ru>
2023-01-17 22:06:36 +0300

    finished refactoring to mongoose

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,16 +32,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
 const db_1 = require("./repositories/db");
 const server_1 = require("./utils/server");
 // remove app to a different folder
 const app = (0, server_1.createServer)();
 const port = 3000;
 const startApp = () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(dotenv);
     console.log(process.env.PORT);
     console.log(process.env.MONGO_URL);
+    yield setTimeout(() => {
+        console.log("timeout");
+    }, 10000);
     yield (0, db_1.runDb)();
     app.listen(port, () => {
         console.log(`App is listening on the port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { config } from "dotenv";
-config();
+import * as dotenv from "dotenv";
+dotenv.config();
 import { runDb } from "./repositories/db";
 import { createServer } from "./utils/server";
 // remove app to a different folder
@@ -7,8 +7,12 @@ const app = createServer();
 const port = 3000;
 
 const startApp = async () => {
+  console.log(dotenv);
   console.log(process.env.PORT);
   console.log(process.env.MONGO_URL);
+  await setTimeout(() => {
+    console.log("timeout");
+  }, 10000);
   await runDb();
   app.listen(port, () => {
     console.log(`App is listening on the port ${port}`);
